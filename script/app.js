c259c3273e2203abf3340e219e49ae3591cc96ae
herculesra <hercules.anselmo@ccc.ufcg.edu.br>
2019-06-19 19:34:17 -0300

    trabalhando na rota de busca de disciplinas

--- script/app.js
+++ script/app.js
@@ -11,14 +11,14 @@ async function cadastro(){
     let email = $email.value;
     let senha = $senha.value;
 
-    const url = "http://localhost:8080/api/users/";
-    // const url = "https://projsof.herokuapp.com/api/users/";
+    // const url = "http://localhost:8080/api/users/";
+    const url = "https://projsof.herokuapp.com/api/user/";
 
     const corpo = {firstName: name, lastName: lname, email: email, password: senha};
     
     const headers = new Headers({
         'Access-Control-Allow-Origin': '*',
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json; charset=utf-8'
     });
         
     const defaults = {
@@ -38,4 +38,51 @@ async function cadastro(){
         alert("Algo deu ruim");
     }
 }
+async function authomatizeRequest(url, method, body){
+    const resposta = null;
+
+    const headers = new Headers({
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json; charset=utf-8'
+    });
+        
+    const defaults = {
+        headers: headers
+    };
+
+    let options = {
+        method: method,
+        body: JSON.stringify(body)};
+    options = Object.assign({}, defaults, options);
+
+    let response = await fetch(url, options);
+    // if(response){ // verifica se o response não é null
+    //     resposta = "Entre no seu email para completar o cadastro";
+    // }else{  // deu erro.
+    //     resposta = "Algo deu ruim";
+    // }
+
+    return response;
+}
+
+// function procuraDisciplina(){  
+//     const $schSubject = document.querySelector("#schSubject");
+//     const $resultSearch = document.querySelector("#resultSearch");
+//     const subString = $schSubject.value;
+//     const resultado = $resultSearch.value;
+
+//     const url = "https://projsof.herokuapp.com/api/subject/search/";
+//     const body = {subString: subString};
+//     const method = "GET";
+
+//     let response = authomatizeRequest(url, method, body);
+
+//     if(response){ //verifica se a response é null
+
+//     }else{
+
+//     }
+//     alert(resposta);
+// }
+
     
