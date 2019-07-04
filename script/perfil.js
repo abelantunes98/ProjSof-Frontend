9c5b860fa1650845ad888d20f61c9efe4171720a
herculesra <hercules.anselmo@ccc.ufcg.edu.br>
2019-07-04 04:32:36 -0300

    foi

--- script/perfil.js
+++ script/perfil.js
@@ -1,22 +1,22 @@
-import {authomatizeRequest} from "./util.js";
-import { viewPerfil,viewComentario} from "./view.js";
+import { authomatizeRequest } from "./util.js";
+import { viewPerfil, viewComentario } from "./view.js";
 
 /**
  * Função responsável por dar o like na disciplina
  */
-async function darLike(){
-    
+async function darLike() {
+
     const url = "https://projsof.herokuapp.com/api/actions/setLike/";
     //const url = "http://localhost:8080/api/actions/setLike/";
     let subject = sessionStorage.getItem("subject");
     subject = JSON.parse(subject);
     let dados;
-    let body = {"like_type":1,"idSubject":subject.id};
+    let body = { "like_type": 1, "idSubject": subject.id };
     let method = 'POST';
 
     let response = await authomatizeRequest(url, method, body);
 
-    if(response.ok){
+    if (response.ok) {
         dados = await response.json();
     }
 
@@ -31,19 +31,19 @@ async function darLike(){
 /**
  * Função Responsável por dar deslike na disciplina
  */
-async function darDeslike(){
-    
+async function darDeslike() {
+
     const url = "https://projsof.herokuapp.com/api/actions/setLike/";
     //const url = "http://localhost:8080/api/actions/setLike/";
     let subject = sessionStorage.getItem("subject");
     subject = JSON.parse(subject);
     let dados;
-    let body = {"like_type":-1,"idSubject":subject.id};
+    let body = { "like_type": -1, "idSubject": subject.id };
     let method = 'POST';
 
     let response = await authomatizeRequest(url, method, body);
 
-    if(response.ok){
+    if (response.ok) {
         dados = await response.json();
     }
 
@@ -55,49 +55,73 @@ async function darDeslike(){
     viewPerfil();
 }
 
-async function enviaComentario(){
+async function enviaComentario() {
     let $mostrar = document.getElementById("mostrar");
     let $comentario = $mostrar.shadowRoot.querySelector("#msgPai");
-    
+
 
     let msg = $comentario.value;
 
     let subject = JSON.parse(sessionStorage.getItem("subject"));
-    
+
     const url = "https://projsof.herokuapp.com/api/comments/postComment/";
-    const corpo = {"id_subject":subject.id, "comment_msg": msg};
+    const corpo = { "id_subject": subject.id, "comment_msg": msg };
     const method = 'POST';
 
-    let response = await authomatizeRequest(url,method,corpo);
+    let response = await authomatizeRequest(url, method, corpo);
 
-    if(response.ok){
+    if (response.ok) {
         let dados = await response.json();
         dados.comments[dados.comments.length - 1].comments_resp = [];
-        sessionStorage.setItem("subject",JSON.stringify(dados));
+        sessionStorage.setItem("subject", JSON.stringify(dados));
         viewPerfil();
-    }else{
+    } else {
         alert("Imporssível enviar comentário");
     }
 
 }
 
-async function deletaComentario(idComentario){
+async function deletaComentario(idComentario) {
 
-    const url = "https://projsof.herokuapp.com/api/comments/deleteComment/"+ idComentario;
+    const url = "https://projsof.herokuapp.com/api/comments/deleteComment/" + idComentario;
     const method = 'DELETE';
     const corpo = null;
 
-    let response = await authomatizeRequest(url,method,corpo);
+    let response = await authomatizeRequest(url, method, corpo);
 
-    if(response.ok){
+    if (response.ok) {
         let dados = await response.json();
-        sessionStorage.setItem("subject",JSON.stringify(dados));
+        sessionStorage.setItem("subject", JSON.stringify(dados));
         viewPerfil();
-    }else{
+    } else {
         alert("Esse comentario não pode ser apagado por você!");
     }
 
 
 }
 
-export {darLike, darDeslike, enviaComentario,deletaComentario};
+async function responderComentario(idcomentario) {
+    let $mostrar = document.getElementById("mostrar");
+    let $comentario = $mostrar.shadowRoot.querySelector("#msgPai");
+
+
+    let msg = $comentario.value;
+
+    let subject = JSON.parse(sessionStorage.getItem("subject"));
+
+    const url = "https://projsof.herokuapp.com/api/comments/postCommentOfAnswer/" + idcomentario;
+    const corpo = { "id_subject": subject.id, "comment_msg": msg };
+    const method = 'POST';
+
+    let response = await authomatizeRequest(url, method, corpo);
+
+    if (response.ok) {
+        let dados = await response.json();
+        sessionStorage.setItem("subject", JSON.stringify(dados));
+        viewPerfil();
+    } else {
+        alert("Imporssível enviar comentário");
+    }
+}
+
+export { darLike, darDeslike, enviaComentario, deletaComentario, responderComentario };
--- script/view.js
+++ script/view.js
@@ -1,28 +1,28 @@
-import { darLike, darDeslike, enviaComentario, deletaComentario} from "./perfil.js";
+import { darLike, darDeslike, enviaComentario, deletaComentario, responderComentario } from "./perfil.js";
 
 
 /**
  * Função para mostrar os ids e nomes das disciplinas.
  * @param  dados - ID's e Nomes das disciplinas ques estão no banco de dados.
  */
-function viewDisciplinas(dados){
+function viewDisciplinas(dados) {
     let $resultSearch = document.querySelector("#resultSearch");
     $resultSearch.innerHTML = '';
     let tamanhoArray = 0;
 
-    if(dados != null) { 
+    if (dados != null) {
         tamanhoArray = dados.length;
     }
 
     //Resolver esse csss
-    if(tamanhoArray == 0 && dados != null){
+    if (tamanhoArray == 0 && dados != null) {
         $resultSearch.innerHTML = "Infelizmente não temos esta disciplina no nosso banco de dados."
     }
-    for(let i = 0; i < tamanhoArray; i++){
+    for (let i = 0; i < tamanhoArray; i++) {
         let html = `<link rel="stylesheet" href="../style/disciplina.css"/>
                     <p class="disciplinas">${dados[i].id} - ${dados[i].subjectName}</p>`
         let novo = document.createElement("div");
-        let shadow = novo.attachShadow({"mode": "open"});
+        let shadow = novo.attachShadow({ "mode": "open" });
         shadow.innerHTML = html;
         $resultSearch.appendChild(novo);
     }
@@ -33,7 +33,7 @@ function viewDisciplinas(dados){
  * @param subject - Disciplina buscada pelo ID
  */
 
-function viewDisciplinaId(subject){
+function viewDisciplinaId(subject) {
     window.sessionStorage.setItem("subject", JSON.stringify(subject));
     let $disciplina = document.getElementById("resultSearch");
     $disciplina.innerHTML = '';
@@ -43,10 +43,10 @@ function viewDisciplinaId(subject){
                     <button id="btn-verPerfil" class="btn btn-efect">Ver Perfil</button>
                 </div>`
 
-    
+
     let novo = document.createElement("div");
-    novo.setAttribute("id","mostrar");
-    let shadow = novo.attachShadow({"mode": "open"});
+    novo.setAttribute("id", "mostrar");
+    let shadow = novo.attachShadow({ "mode": "open" });
     shadow.innerHTML = html;
     $disciplina.appendChild(novo);
     novo.shadowRoot.querySelector("#btn-verPerfil").onclick = viewPerfil;
@@ -55,8 +55,8 @@ function viewDisciplinaId(subject){
 /**
  * Função responsável por mostrar o Perfil da disciplina.
  */
-function viewPerfil(){
-   
+function viewPerfil() {
+
     let subject = sessionStorage.getItem("subject")
     subject = JSON.parse(subject);
     let $resultado = document.getElementById("resultSearch");
@@ -71,12 +71,12 @@ function viewPerfil(){
                     <button id="btn-deslike" class="btn btn-efect material-icons md-light">thumb_down</button>
                     <div id="comentario">
                         <input id="msgPai" type="text" placeholder="Insira um comentário." />
-                        <button id="btn-msgPai">Enviar</button>
+                        <button id="btn-msgPai" class="btn">Enviar</button>
                     </div>
                 </div>`
     let novo = document.createElement("div");
-    novo.setAttribute("id","mostrar");
-    let shadow = novo.attachShadow({"mode":"open"});
+    novo.setAttribute("id", "mostrar");
+    let shadow = novo.attachShadow({ "mode": "open" });
     shadow.innerHTML = html;
     $resultado.appendChild(novo);
     novo.shadowRoot.querySelector("#btn-like").onclick = darLike;
@@ -86,55 +86,55 @@ function viewPerfil(){
 }
 
 
-function viewComentario(){
+function viewComentario() {
     let $divComent = document.getElementById("resultComentario");
     let subject = sessionStorage.getItem("subject");
     let comentarios = JSON.parse(subject).comments;
     let html = "";
     let idsBotoes = new Array();
-    for(let i=0; i < comentarios.length; i++){
+    for (let i = 0; i < comentarios.length; i++) {
         let tamanho_resp = comentarios[i].comments_resp.length;
-        let id_button = "btn-comentPai"+comentarios[i].id;
-        let respComments = "btn-comentsResp"+comentarios[i].id;
+        let id_button = "btn-comentPai" + comentarios[i].id;
+        let respComments = "btn-comentsResp" + comentarios[i].id;
         idsBotoes.push([id_button, comentarios[i].id, respComments]);
         html += `
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
                     <div class="comentarioPai">
-                    <p>${comentarios[i].comment_msg}</p>
-                    <button id="${id_button}" class="material-icons">delete</button>
-                    <h4>${comentarios[i].user_name}</h4>
-                    <time>${comentarios[i].data}</time>
-                    <button id="${respComments}">Respostas</button>
+                    <p id="msg-pai">${comentarios[i].comment_msg}</p>
+                    <p id="nome-usuario">${comentarios[i].user_name}</p>
+                    <button id="${id_button}" class="btn-apagar material-icons">delete</button>
+                    <time id="hora">Em: ${comentarios[i].data}</time>
+                    <button id="${respComments}" class="btn-responder">Respostas</button>
                     </div>`
-      
-        }
-        //html += `</div>`    
+
+    }
+
     $divComent.innerHTML = html;
 
     idsBotoes.forEach(idVez => {
-        document.getElementById(idVez[0]).onclick = (() => {deletaComentario(idVez[1])});
-        document.getElementById(idVez[2]).onclick = (() => {viewRespComentario(idVez[1])});    
-    }); 
+        document.getElementById(idVez[0]).onclick = (() => { deletaComentario(idVez[1]) });
+        document.getElementById(idVez[2]).onclick = (() => { viewRespComentario(idVez[1]) });
+    });
 }
-    
-    function viewRespComentario(commentId){
-        let $divComent = document.getElementById("resultComentario");
-        let idsBotoes = new Array();
-        let subject = sessionStorage.getItem("subject");
-        let comentarios = JSON.parse(subject).comments;
-        let html = `<button id="voltar" class="material-icons">undo</button>`;
-        let comment;
-        comentarios.forEach(commentVez => {
-            if (commentVez.id == commentId) {
-                comment = commentVez;
-            }
-        });
-        let commentsResp = comment.comments_resp;
-        let tamanho_resp = commentsResp.length;
-        for(let i=0; i < tamanho_resp; i++){
-            let id_button = "btn-comentFilho"+commentsResp[i].id;
-            idsBotoes.push([id_button, commentsResp[i].id]);
-            html += `
+
+function viewRespComentario(commentId) {
+    let $divComent = document.getElementById("resultComentario");
+    let idsBotoes = new Array();
+    let subject = sessionStorage.getItem("subject");
+    let comentarios = JSON.parse(subject).comments;
+    let html = `<button id="voltar" class="material-icons">undo</button>`;
+    let comment;
+    comentarios.forEach(commentVez => {
+        if (commentVez.id == commentId) {
+            comment = commentVez;
+        }
+    });
+    let commentsResp = comment.comments_resp;
+    let tamanho_resp = commentsResp.length;
+    for (let i = 0; i < tamanho_resp; i++) {
+        let id_button = "btn-comentFilho" + commentsResp[i].id;
+        idsBotoes.push([id_button, commentsResp[i].id]);
+        html += `
             <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
                         <div class="comentarioFilho">
                         <p>${commentsResp[i].comment_msg}</p>
@@ -142,16 +142,17 @@ function viewComentario(){
                         <h4>${commentsResp[i].user_name}</h4>
                         <time>${commentsResp[i].data}</time>
                         </div>`
-    
-        }
-        
-        $divComent.innerHTML = html;
 
-        idsBotoes.forEach(idVez => {
-            document.getElementById(idVez[0]).onclick = (() => {deletaComentario(idVez[1])});
-        }); 
-        document.getElementById("voltar").onclick = (() => {viewPerfil()});    
-    }   
+    }
+
+    $divComent.innerHTML = html;
+
+    idsBotoes.forEach(idVez => {
+        document.getElementById(idVez[0]).onclick = (() => { deletaComentario(idVez[1]) });
+    });
+    document.getElementById("voltar").onclick = (() => { viewPerfil() });
+    document.getElementById("btn-msgPai").onclick = (() => { responderComentario(commentId) });
+}
 
 
-export {viewDisciplinas, viewDisciplinaId, viewPerfil, viewComentario};
+export { viewDisciplinas, viewDisciplinaId, viewPerfil, viewComentario };
